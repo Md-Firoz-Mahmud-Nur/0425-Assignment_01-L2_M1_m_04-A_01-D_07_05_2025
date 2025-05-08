a7149466a6acb65214277dd772d0715da7237e7a
firoz.nur <firoznur5@gmail.com>
2025-05-08 16:44:07 +0600

    Solve : Problem 04

--- src/index.ts
+++ src/index.ts
@@ -31,12 +31,46 @@ const books = [
 
 // P3
 
-function concatenateArrays<T>(...arrays: T[][]): T[]{
+function concatenateArrays<T>(...arrays: T[][]): T[] {
   return arrays.reduce((acc, curr) => acc.concat(curr), []);
-};
+}
 
-concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
-concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+// concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+// concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
 
-console.log(concatenateArrays(["a", "b"], ["c"]));
-console.log(concatenateArrays([1, 2], [3, 4], [5]));
+// console.log(concatenateArrays(["a", "b"], ["c"]));
+// console.log(concatenateArrays([1, 2], [3, 4], [5]));
+
+// P4
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+  getInfo() {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
+
+  getModel() {
+    return `Model: ${this.model}`;
+  }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+// myCar.getModel();  // Output: "Model: Corolla"
+
+// console.log(myCar.getInfo());
+// console.log(myCar.getModel());
