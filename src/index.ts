4bd445591e62294d3ac2ff52048924b4fcb83697
firoz.nur <firoznur5@gmail.com>
2025-05-08 16:07:38 +0600

    Solve : Problem 01

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// P1
+
+function formatString(input: string, toUpper?: boolean): string {
+  console.log("toUpper--", toUpper);
+  if (toUpper === false) {
+    return input.toLowerCase();
+  }
+  return input.toUpperCase();
+}
+
+console.log(formatString("Hello")); // Output: "HELLO"
+console.log(formatString("Hello", true)); // Output: "HELLO"
+console.log(formatString("Hello", false)); // Output: "hello"
