c9cb68632bb3ca6fa6b6af3033f0562cb24fc7fc
firoz.nur <firoznur5@gmail.com>
2025-05-08 16:31:16 +0600

    Solve : Problem 02

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 // P1
 
 function formatString(input: string, toUpper?: boolean): string {
-  console.log("toUpper--", toUpper);
+  // console.log("toUpper--", toUpper);
   if (toUpper === false) {
     return input.toLowerCase();
   }
   return input.toUpperCase();
 }
 
-console.log(formatString("Hello")); // Output: "HELLO"
-console.log(formatString("Hello", true)); // Output: "HELLO"
-console.log(formatString("Hello", false)); // Output: "hello"
+// console.log(formatString("Hello")); // Output: "HELLO"
+// console.log(formatString("Hello", true)); // Output: "HELLO"
+// console.log(formatString("Hello", false)); // Output: "hello"
+
+// P2
+
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
